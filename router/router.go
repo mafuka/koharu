63fc438dc47da497d88942b9e0f4e0252f8df19b
Kwaain <kwaain@outlook.com>
2023-09-04 19:09:50 +0800

    feat(middleware): 中间件式消息处理

--- router/router.go
+++ router/router.go
@@ -5,10 +5,13 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kwaain/nakisama/middleware"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(middleware.Echo())
 
 	router.GET("/", func(c *gin.Context) {
 		time.Sleep(5 * time.Second)

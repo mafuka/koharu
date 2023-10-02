ad270ea1edf17f5a5b6a75b6dfc8d3bf4c4d48b1
Kwaain <kwaain@outlook.com>
2023-10-03 01:47:11 +0800

    feat(api): 追加 GetLoginInfo 接口

--- api/account.go
+++ api/account.go
@@ -1,3 +1,4 @@
+// api 包含了对 OneBot HTTP API 的封装。
 package api
 
 import (
@@ -7,13 +8,15 @@ import (
 	"github.com/kwaain/nakisama/lib/request"
 )
 
-// LoginInfo 是 GetLoginInfo 的返回结构体
+// LoginInfo 包含机器人的登录账号信息。
 type LoginInfo struct {
-	UserID   int64  `json:"user_id"`
-	Nickname string `json:"nickname"`
+	UserID   int64  `json:"user_id"`  // QQ 账号
+	Nickname string `json:"nickname"` // 昵称
 }
 
-// GetLoginInfo 获取登录号信息, 返回 LoginInfo 结构体.
+// GetLoginInfo 获取机器人的登录账号信息。
+//
+// 该接口无需参数。调用成功则返回 LoginInfo 结构体。
 func GetLoginInfo() (LoginInfo, error) {
 	response, err := request.Send("/get_login_info", "POST", nil)
 	if err != nil {
@@ -22,7 +25,7 @@ func GetLoginInfo() (LoginInfo, error) {
 
 	data, err := json.Marshal(response.Data)
 	if err != nil {
-		return LoginInfo{}, fmt.Errorf("将 data 字段转换为 JSON 失败: %v", err)
+		return LoginInfo{}, fmt.Errorf("解析响应失败: %v", err)
 	}
 
 	var loginInfo LoginInfo
